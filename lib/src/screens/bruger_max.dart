d041d8902d947310c22fa7554cb55199f53605c9
juungchull <89003952+juungchull@users.noreply.github.com>
2022-12-29 16:48:16 +0900

    burger

--- lib/src/screens/bruger_max.dart
+++ lib/src/screens/bruger_max.dart
@@ -6,12 +6,36 @@ class BrugerMax extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          child: Text(
-            'burger max',
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * 0.3,
+                color: Colors.red,
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 50),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CircleAvatar(),
+                    CircleAvatar(),
+                  ],
+                ),
+              ),
+              Positioned(
+                left: 50,
+                top: 100,
+                child: Container(
+                  color: Colors.purple,
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  height: MediaQuery.of(context).size.height * 0.3,
+                ),
+              )
+            ],
           ),
-        ),
+        ],
       ),
     );
   }
